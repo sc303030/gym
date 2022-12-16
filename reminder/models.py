769eb5216a1923e003c9918012a868410875c979
sc303030 <lkh6035@naver.com>
2022-12-16 20:01:41 +0900

    22.12.16 | add max_length

--- reminder/models.py
+++ reminder/models.py
@@ -5,7 +5,7 @@ from model_utils.models import TimeStampedModel
 # Create your models here.
 
 class Reminder(TimeStampedModel):
-    school = models.CharField()
+    school = models.CharField(max_length=30)
     url = models.TextField()
-    date = models.CharField()
-    css_selector = models.CharField()
+    date = models.CharField(max_length=10)
+    css_selector = models.TextField()
