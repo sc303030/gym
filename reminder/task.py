b390ab97633e13c77e09493e018cc597aff98e31
sc303030 <lkh6035@naver.com>
2022-12-20 21:24:40 +0900

    22.12.20 | add celery_task

--- reminder/task.py
+++ reminder/task.py
@@ -1,6 +1,14 @@
 from celery import shared_task
+from reminder.crawling.create_reminder import CreateReminder
+from typing import Optional
 
 
 @shared_task
-def test():
-    print('들어옴')
+def create_reminder_worker(school: str) -> Optional[str]:
+    obj = CreateReminder(name=school)
+    title, date = obj.crawling()
+    if title:
+        obj.create_reminder(title, date)
+        return "201"
+    else:
+        return None
