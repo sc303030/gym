d26ff91b11c2ec053ac800bf459b85aee125da35
sc303030 <lkh6035@naver.com>
2022-12-23 19:48:25 +0900

    22.12.23 | school list get models

--- reminder/tasks.py
+++ reminder/tasks.py
@@ -2,7 +2,7 @@ from celery import shared_task
 from reminder.crawling.create_reminder import CreateReminder
 from reminder.kakao.save_token import refresh_token
 from reminder.kakao.send_reminder import send_reminder
-from reminder.models import Reminder
+from reminder.models import Reminder, School
 
 
 @shared_task
@@ -14,8 +14,8 @@ def create_reminder_worker(school: str) -> None:
 
 @shared_task
 def start_crawling():
-    school_list = ['사당중학교', '봉은중학교']
-    for school in school_list:
+    schools = School.objects.all().values_list('name', flat=True)
+    for school in schools:
         create_reminder_worker.delay(school)
     reminders = Reminder.objects.filter(remind=False)
     for reminder in reminders:
